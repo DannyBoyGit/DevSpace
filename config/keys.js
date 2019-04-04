95205c5641979a12b227990caa38f90a776e22c4
Danny <salas.danny2013@gmail.com>
2019-04-03 21:53:37 -0500

    JWT tokens have been added to successful login

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://Danny:TownView21@cluster0-23jc3.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://Danny:TownView21@cluster0-23jc3.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
